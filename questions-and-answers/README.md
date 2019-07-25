2a49ac5f6f22ef200f058f1f6212a13350c34833
peakergzf <peakergzf@outlook.com>
2019-07-25 16:12:19 +1000

    fix nba playoff rule change and reconstruct code

--- questions-and-answers/README.md
+++ questions-and-answers/README.md
@@ -2,10 +2,10 @@
 
 ## Question 1
 
-Given the number of NBA playoff games a player played in a season, can we (if yes, how to) determine which rounds (R1, CSF, CF, F) his team advanced to?
+Given the number of NBA playoff games a team played in a season, can we (if yes, how to) determine which rounds (R1, CSF, CF, F) the team advanced to?
 
 For example, the highlighted column in the table below (from [here](https://www.basketball-reference.com/players/d/duncati01.html)) 
-shows the number of playoff games Tim Duncan played in each season of his career.
+shows the number of playoff games Tim Duncan played in each season of his career after 2002 (R1 was extended to a best-of-seven series in 2003).
 ![Tim Duncan stats](../images/tim_duncan_stats.PNG)
 
 It turns out that some of the numbers can, others don't, see the codes for more details.
--- questions-and-answers/playoff_games_cnt.py
+++ questions-and-answers/playoff_games_cnt.py
@@ -2,22 +2,26 @@
 Question:
 given the number of NBA playoff games a team played in a season,
 can we (if yes, how to) determine which rounds(R1, CSF, CF, F) the team advanced to?
-
-
 """
 
-N = 8  # the number of rounds (N = 4 in NBA)
+N = 4  # the number of rounds (N = 4 in NBA)
 
-# games_cnt[i] = a list of all possible number of games a team plays up to round i
-games_cnt = [[j for j in range(4 * i, 7 * i + 1)] for i in range(N)]
+# games_cnt[i] = a list of all possible number of games a team plays up to round i (1 <= i <= N)
+games_cnt = [[j for j in range(4 * i, 7 * i + 1)] for i in range(N + 2)]
 
-for i in range(1, N - 1):
+for i in range(1, N + 1):
     for j in range(len(games_cnt[i])):
-        # only print unique numbers
-        if (games_cnt[i][j] in games_cnt[i - 1]) or (games_cnt[i][j] in games_cnt[i + 1]):
-            print("-", end="\t")
-        else:
+        # R1 (no rounds before)
+        if i == 1 and (games_cnt[i][j] not in games_cnt[i + 1]):
             print(games_cnt[i][j], end="\t")
+        # F (no rounds after)
+        elif i == N and (games_cnt[i][j] not in games_cnt[i - 1]):
+            print(games_cnt[i][j], end="\t")
+        # CSF, CF (have rounds before and after)
+        elif (games_cnt[i][j] not in games_cnt[i - 1]) and (games_cnt[i][j] not in games_cnt[i + 1]):
+            print(games_cnt[i][j], end="\t")
+        else:
+            print("-", end="\t")
     print()
 print()
 
@@ -26,9 +30,16 @@ such number x exists when
 max # of games up to the previous round < x < min # of games up to the next round
 
 we also know that x won't exist after round 3 because: let i be the i-th round, 
-if 7*(i-1) < 4*(i+1) then i < 11/3 < 4
+if 7*(i-1) < 4*(i+1) then i < 11/3 < 4 (but in NBA there is no such restriction for round 4 (final))
 """
-for i in range(1, N):
+for i in range(1, N + 1):
     for x in range(4 * i, 7 * i + 1):
-        if 7 * (i - 1) < x < 4 * (i + 1):
+        # R1 (no rounds before)
+        if i == 1 and x < 4 * (i + 1):
+            print(x, end=", ")
+        # F (no rounds after)
+        elif i == N and x > 7 * (i - 1):
+            print(x, end=", ")
+        # CSF, CF (have rounds before and after)
+        elif 7 * (i - 1) < x < 4 * (i + 1):
             print(x, end=", ")
